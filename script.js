eda85f58a6a7f7b371f34b8ab54dfb3069958cd9
samoradeng <dengsamora@gmail.com>
2023-10-27 11:38:15 -0700

    Update script.js
    added lives, stats and ability to share on twitter/x.com

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    const API_KEY = "zl2lkL1rpY7gOv6uCtIYM4BKWAp81eHOnIQT7Lbc";
+    const API_KEY = "lGBbDFd82zkgNzrKEY3KYCDGI4AEdNST01U6Q57I";
     let countries = [];
     let currentCountry = null;
     let usedCountries = [];
     let score = 0;
     let total = 0;
+    let gameState = "playing"; // possible values: "playing", "over"
+    let playedDates = []; // Added this for unique days tracking
+
+    if (localStorage.getItem('playedDates')) {
+        playedDates = JSON.parse(localStorage.getItem('playedDates'));
+    }
+
+    let overallScore = 0; // Introduce this variable
+    let overallTotal = 0; // Introduce this variable
+
+
+    // User statistics
+    let timesPlayed = 0;
+    let currentStreak = 0;
+    let maxStreak = 0;
+    let lives = 3;
+    const hearts = document.querySelectorAll('.heart-icon');
+
 
     // DOM Elements
     const flagImg = document.getElementById('flag');
@@ -15,7 +32,79 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextBtn = document.getElementById('next');
     const scoreDisplay = document.getElementById('score');
     const headingText = document.getElementById('heading');
-    const subHeadingText = document.getElementById('subHeading')
+    const subHeadingText = document.getElementById('subHeading');
+    const statsBtn = document.getElementById('stats-btn');
+    const statsModal = document.getElementById('stats-modal');
+    const closeBtn = document.querySelector('.close-btn');
+
+    // Check if today's date is already in playedDates
+    const today = (new Date()).toDateString();
+    if (!playedDates.includes(today)) {
+        playedDates.push(today);
+        // Also, we'll update streaks here:
+        const streaks = calculateStreaks(playedDates);
+        currentStreak = streaks.currentStreak;
+        maxStreak = streaks.maxStreak;
+    }
+
+    function calculateStreaks(dates) {
+        const oneDay = 24 * 60 * 60 * 1000;  // milliseconds in a day
+
+        let currentStreak = 1;  // We start at 1 because the streak includes today.
+        let maxStreak = 0;
+        let streak = 1;  // The streak for the loop also starts at 1 for the same reason.
+
+        dates.sort((a, b) => new Date(a) - new Date(b)); // Sort the dates in ascending order
+
+        for (let i = 1; i < dates.length; i++) {
+            const prevDate = new Date(dates[i - 1]);
+            const currentDate = new Date(dates[i]);
+
+            if ((currentDate - prevDate) === oneDay) {
+                currentStreak++;
+            } else {
+                maxStreak = Math.max(maxStreak, currentStreak); // Update maxStreak if currentStreak was greater.
+                currentStreak = 1;
+            }
+
+            // Set the time of both dates to midnight for accurate comparison
+            prevDate.setHours(0, 0, 0, 0);
+            currentDate.setHours(0, 0, 0, 0);
+
+            if (Math.abs(currentDate - prevDate) === oneDay) {
+                streak++;
+            } else {
+                if (streak > maxStreak) {
+                    maxStreak = streak;
+                }
+                streak = 1;
+            }
+
+            if (i === dates.length - 1) {
+                currentStreak = streak;
+                if (streak > maxStreak) {
+                    maxStreak = streak;
+                }
+            }
+        }
+
+        // After loop ends, it's possible that the longest streak is the current one.
+        maxStreak = Math.max(maxStreak, currentStreak);
+        return { currentStreak, maxStreak };
+    }
+
+
+
+    // Event Listeners
+    options.forEach(button => button.addEventListener('click', checkAnswer));
+    nextBtn.addEventListener('click', nextCountry);
+    statsBtn.addEventListener('click', showStatsModal);
+    closeBtn.addEventListener('click', closeStatsModal);
+    window.addEventListener('click', function (event) {
+        if (event.target === statsModal) {
+            statsModal.style.display = 'none';
+        }
+    });
 
     // Fetch countries data
     fetch(`https://countryapi.io/api/all?apikey=${API_KEY}`)
@@ -26,15 +115,135 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Error fetching data:', error));
 
-    // Event Listeners
-    options.forEach(button => button.addEventListener('click', checkAnswer));
-    nextBtn.addEventListener('click', nextCountry);
+
+    function loadGameState() {
+        if (localStorage.getItem('gameEndDate') && isNewDay(localStorage.getItem('gameEndDate'))) {
+            // ... reset the game and remove the gameEndDate from local storage...
+            localStorage.removeItem('gameEndDate');
+            restartGame(); // This function should reset everything for a new game
+
+            overallScore = gameStateData.overallScore || 0;
+            overallTotal = gameStateData.overallTotal || 0;
+            return;  // No need to load the saved data if we are starting fresh
+
+
+
+        }
+
+        const savedData = localStorage.getItem('countryGame');
+
+        if (savedData) {
+            const gameStateData = JSON.parse(savedData);
+
+            gameState = gameStateData.gameState;
+            score = gameStateData.score;
+            total = gameStateData.total;
+            lives = gameStateData.lives;
+            timesPlayed = gameStateData.timesPlayed;
+            currentStreak = gameStateData.currentStreak || 0;
+            maxStreak = gameStateData.maxStreak || 0;
+            usedCountries = gameStateData.usedCountries;
+
+            overallScore = gameStateData.overallScore;
+            overallTotal = gameStateData.overallTotal;
+
+            // Update the UI with the loaded data
+            scoreDisplay.textContent = "Score: " + score + "/" + total;
+            for (let i = 0; i < 3 - lives; i++) {
+                hearts[i].classList.add('heart-lost');
+
+                if (lives === 0) {
+                    document.getElementById('game-over-screen').style.display = 'block';
+                    // Hide other game elements as appropriate...
+                    headingText.style.display = 'none';
+                    subHeadingText.style.display = 'none';
+                    flagImg.style.display = 'none';
+                    document.getElementById('options').style.display = 'none';
+                }
+
+
+            }
+
+        }
+
+    }
+    loadGameState();
+
+    document.getElementById('see-stats-from-gameover').addEventListener('click', showStatsModal);
+
+
+    function isNewDay(savedDate) {
+        const currentDate = new Date().toDateString();
+        return currentDate !== savedDate;
+    }
+
+    function restartGame() {
+        nextBtn.disabled = false;
+        lives = 3;
+        score = 0;
+        total = 0;
+        gameState = "playing";
+        usedCountries = [];
+        // Reset UI elements
+        document.getElementById('game-over-screen').style.display = 'none';
+        headingText.style.display = 'block';
+        subHeadingText.style.display = 'block';
+        flagImg.style.display = 'block';
+        document.getElementById('options').style.display = 'block';
+        hearts.forEach(heart => heart.classList.remove('heart-lost'));
+        // Now, fetch a new country
+        nextCountry();
+    }
+
+
+
+    function showStatsModal() {
+        updateStats();
+        statsModal.style.display = 'block';
+    }
+
+    function closeStatsModal() {
+        statsModal.style.display = 'none';
+    }
+
+    function updateStats() {
+        document.getElementById('times-played').textContent = playedDates.length;
+        document.getElementById('current-streak').textContent = currentStreak;
+        document.getElementById('max-streak').textContent = maxStreak;
+        // For percentage right:
+        const percentageRight = (score / total) * 100;
+
+        const overallPercentageRight = overallTotal === 0 ? 0 : (overallScore / overallTotal) * 100;
+        const recentPercentageRight = total === 0 ? 0 : (score / total) * 100;
+        document.getElementById('percentage-daily').textContent = `${recentPercentageRight.toFixed(0)}%`;
+        // document.getElementById('percentage-overall').textContent = `Overall: ${overallPercentageRight.toFixed(0)}%`;
+    }
+
+    // Function to share score on Twitter
+    function shareOnTwitter() {
+        const text = `I scored ${score} on the flaggy game! Try it out now!`;
+        const url = 'https://yourquizapp.com'; // Replace with your app's URL
+        window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`, '_blank');
+    }
+
+    document.getElementById('share-button').addEventListener('click', shareOnTwitter);
 
     function nextCountry() {
+        nextBtn.disabled = false;
+        saveGameState();
+        if (gameState === "over") return;
+        timesPlayed++;
+
         if (countries.length === 0) {
-            alert("Game Over! Your score: " + score + "/" + total);
+            document.getElementById('final-score-display').textContent = score + "/" + total;
+            document.getElementById('game-over-overlay').style.display = 'flex';
             return;
         }
+
+        document.getElementById('close-overlay-btn').addEventListener('click', function () {
+            document.getElementById('game-over-overlay').style.display = 'none';
+        });
+
 
         const randomIndex = Math.floor(Math.random() * countries.length);
         currentCountry = countries[randomIndex];
@@ -55,6 +264,64 @@ document.addEventListener("DOMContentLoaded", function () {
             button.classList.remove('disabled', 'correct-answer', 'wrong-answer'); // Remove disabled and styling classes
         });
     }
+
+
+    function loseLife() {
+        if (lives > 0) {
+            lives--;
+            hearts[lives].classList.add('heart-lost');
+            saveGameState();
+        }
+
+        if (lives === 0) {
+            gameState = "over";
+            nextBtn.disabled = true;
+            setTimeout(() => {
+                //alert('Thanks for playing!');
+                document.getElementById('final-score-display').textContent = score + "/" + total;
+                document.getElementById('game-over-overlay').style.display = 'flex';
+                showStatsModal();
+            }, 1000);
+        }
+        saveGameState();
+    }
+
+
+    function saveGameState() {
+
+        overallScore += score; // Update the overall score with the current game's score
+        overallTotal += total; // Update the overall total with the current game's total
+        // Always ensure streaks are updated before saving the game state
+        const streaks = calculateStreaks(playedDates);
+        currentStreak = streaks.currentStreak;
+        maxStreak = streaks.maxStreak;
+
+
+
+        const gameStateData = {
+            gameState: gameState,
+            score: score,
+            total: total,
+            lives: lives,
+            timesPlayed: timesPlayed,
+            currentStreak: currentStreak,
+            maxStreak: maxStreak,
+            usedCountries: usedCountries,
+            playedDates: playedDates,
+
+            overallScore: overallScore,
+            overallTotal: overallTotal,
+        };
+
+        localStorage.setItem('countryGame', JSON.stringify(gameStateData));
+        localStorage.setItem('playedDates', JSON.stringify(playedDates));
+
+        localStorage.setItem('gameEndDate', new Date().toDateString());
+    }
+
+
+
+
 
     function updateOptions() {
         const allOtherCountries = [...countries, ...usedCountries].filter(country => country.name !== currentCountry.name);
@@ -83,11 +350,25 @@ document.addEventListener("DOMContentLoaded", function () {
         if (selectedCountryName === currentCountry.name) {
             message.textContent = "🎉 Correct! Well done!";
             score++;
+            currentStreak++;
+            overallScore++;
+            if (currentStreak > maxStreak) {
+                maxStreak = currentStreak;
+            }
+            else {
+                currentStreak = 0;
+                const streaks = calculateStreaks(playedDates);
+                maxStreak = streaks.maxStreak;
+            }
+
             event.target.classList.add('correct-answer');
-            // createConfetti();
         } else {
             message.textContent = "😢 Oops, that's not correct.";
             event.target.classList.add('wrong-answer');
+            currentStreak = 0;
+            loseLife();
+            saveGameState();
+
         }
 
         options.forEach(button => {
@@ -97,6 +378,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         total++;
+        overallTotal++;
         scoreDisplay.textContent = "Score: " + score + "/" + total;
         showFacts(currentCountry);
         nextBtn.hidden = false;
@@ -104,23 +386,24 @@ document.addEventListener("DOMContentLoaded", function () {
         subHeadingText.hidden = true;
     }
 
-    /*function createConfetti() {
-        confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 }
-        });
-    }*/
-
     function showFacts(country) {
         facts.innerHTML = `
             <h2>${country.name}</h2>
+            <p class="fact-text"><strong>Capital:</strong> ${country.capital}</p>
             <p class="fact-text"><strong>Region:</strong> ${country.region}</p>
-            <p class="fact-text"><strong>Subregion:</strong> ${country.subregion}</p>
-            <p class="fact-text"><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-            <p class="fact-text"><strong>Languages:</strong> ${Object.values(country.languages).join(', ')}</p>
-            <p class="fact-text"><strong>Currency:</strong> ${country.currencies[Object.keys(country.currencies)[0]].name} (${country.currencies[Object.keys(country.currencies)[0]].symbol})</p>
+            <p class="fact-text"><strong>Sub Region:</strong> ${country.subregion}</p>
+            <p class="fact-text"><strong>Population:</strong> ${country.population.toLocaleString('en-US')}</p>
+            
         `;
         facts.hidden = false;
     }
+    console.log("Current Streak:", currentStreak);
+    console.log("Max Streak:", maxStreak);
+    console.log("Overall Score:", overallScore);
+    console.log("Overall Total:", overallTotal);
+    //console.log("Percentage:", overallPercentageRight);
+
 });
+
+
+
